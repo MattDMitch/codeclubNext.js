6e19775ae50fd551cb569b1f1ff30ff93c37d59a
mattdmitch <matthew@theaardvark.com>
2024-05-14 13:35:16 -0500

    api changed

--- my-next-app/src/app/api/random.ts
+++ my-next-app/src/app/api/random.ts
@@ -1,8 +1,9 @@
-export default function handler(req, res) {
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
     // Generate a random number between 1 and 100
     const randomNumber = Math.floor(Math.random() * 100) + 1;
 
     // Send a JSON response with the random number
     res.status(200).json({ randomNumber });
 }
-
