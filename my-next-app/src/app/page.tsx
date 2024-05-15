db655bba60a9d0a3df7d8df33e82fafd8bf66d1f
MattDMitch <91271596+MattDMitch@users.noreply.github.com>
2024-05-15 10:49:28 -0500

    Update page.tsx

--- my-next-app/src/app/page.tsx
+++ my-next-app/src/app/page.tsx
@@ -1,14 +1,35 @@
-// src/app/page.tsx
-import { getRandomResponse } from './utils/randomResponse';
+import React from 'react';
 
-let HomePage: React.FC = async () => {
-  let randomResponse = getRandomResponse();
-  
-  return (
-    <div>
-      <h1>{randomResponse}</h1>
-    </div>
-  );
-};
+class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      randomResponse: '',
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const response = await fetch('https://randoresponse.matthew-4b1.workers.dev/');
+      if (response.ok) {
+        const randomResponse = await response.text();
+        this.setState({ randomResponse });
+      } else {
+        console.error('Failed to fetch random response:', response.status);
+      }
+    } catch (error) {
+      console.error('Error fetching random response:', error);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Random Response:</h1>
+        <p>{this.state.randomResponse}</p>
+      </div>
+    );
+  }
+}
 
 export default HomePage;
